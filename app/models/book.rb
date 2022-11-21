489a9cdcd356a65c4b39e78b02b75095f861b3b8
EC2 Default User <ec2-user@ip-172-31-36-94.ap-northeast-1.compute.internal>
2022-11-21 12:38:21 +0000

    Lest

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
 
   has_one_attached image
+  belongs_ :user
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :books, only: [:new, :index, :show]
+  resources :users, only: [:show, :edit]
   get '/homes/about' => 'homes#about', as: 'about'
 end
