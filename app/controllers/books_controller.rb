e038e8404e5d3f58490d5ee54edc07b6f0558f25
EC2 Default User <ec2-user@ip-172-31-36-94.ap-northeast-1.compute.internal>
2022-11-22 12:44:43 +0000

    Lest

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,16 @@
 class BooksController < ApplicationController
 
   def new
-    @book = Book.new
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
   end
+  
+  
+  
 end
